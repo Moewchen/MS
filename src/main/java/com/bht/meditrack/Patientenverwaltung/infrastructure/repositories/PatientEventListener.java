05fb4ba974ab80c9a63239d059441aeb0d3b2536
Zhasmina Dimova <zhasmina.dimova@adesso.de>
2025-01-18 15:41:29 +0100

    clear

--- src/main/java/com/bht/meditrack/Patientenverwaltung/infrastructure/repositories/PatientEventListener.java
+++ src/main/java/com/bht/meditrack/Patientenverwaltung/infrastructure/repositories/PatientEventListener.java
@@ -6,24 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-//@Component
-//@Slf4j
-//public class PatientEventListener {
-//    @EventListener
-//    public void handlePatientAngelegt(PatientAngelegtEvent event) {
-//        log.info("Neuer Patient angelegt: ID={}, Personendata={}, Kontaktdaten={}, " +
-//                        "Krankenkasse={}, Krankenversicherungsnummer={}, Adresse={}, Angelegt am={}",
-//                event.getPatientId(),
-//                event.getPersonendaten(),
-//                event.getKontaktdaten(),
-//                event.getKrankenkasse(),
-//                event.getKrankenversicherungsnummer(),
-//                event.getAdresse(),
-//                event.getCreatedAt()
-//        );
-//    }
-//}
-
 
 @Component
 @Slf4j
