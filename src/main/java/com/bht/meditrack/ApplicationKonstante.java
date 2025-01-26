440742d72f8f7b45034da8d8982aa167e357bc93
Jana Hesse <84595273+Moewchen@users.noreply.github.com>
2025-01-26 23:43:35 +0100

    Refactor

--- src/main/java/com/bht/meditrack/ApplicationKonstante.java
+++ src/main/java/com/bht/meditrack/ApplicationKonstante.java
@@ -4,8 +4,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class ApplicationKonstante {
-    private ApplicationKonstante() {
-    }
     public static final String API_VERSION = "/api/v1";
 
 }
